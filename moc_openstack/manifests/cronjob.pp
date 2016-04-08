f32480a264adb96e2b9f3b0660a2b9e180424248
root <root@dev-foreman1.staging.moc.edu>
2016-04-08 17:15:59 -0400

    used variables instead of hardcoded paths

--- moc_openstack/manifests/cronjob.pp
+++ moc_openstack/manifests/cronjob.pp
@@ -32,31 +32,31 @@ class moc_openstack::cronjob (
 
   # backup the original redhat.repo before puppet run
   exec {'backup_redhat_repo':
-    onlyif  => "/bin/test -f /etc/yum.repos.d/redhat.repo",
-    command => "/bin/cp /etc/yum.repos.d/redhat.repo /etc/yum.repos.d/redhat.repo.default",
+    onlyif  => "/bin/test -f ${base_dir}redhat.repo",
+    command => "/bin/cp ${base_dir}redhat.repo ${base_dir}redhat.repo.default",
   } ->
   exec {'disable_redhat_repos':
-    onlyif  => "/bin/test -f /etc/yum.repos.d/redhat.repo",
-    command => "/bin/sed -i '/enabled/c\enabled = 0 ' /etc/yum.repos.d/redhat.repo",
+    onlyif  => "/bin/test -f ${base_dir}redhat.repo",
+    command => "/bin/sed -i '/enabled/c\enabled = 0 ' ${base_dir}redhat.repo",
   } ->
   exec {'disable_epel_repos':
-    onlyif  => "/bin/test -f /etc/yum.repos.d/epel.repo",
-    command => "/bin/sed -i '/enabled/c\enabled = 0 ' /etc/yum.repos.d/epel.repo",
+    onlyif  => "/bin/test -f ${base_dir}epel.repo",
+    command => "/bin/sed -i '/enabled/c\enabled = 0 ' ${base_dir}epel.repo",
   } ->
   exec {'disable_epel_testing_repos':
-    onlyif  => "/bin/test -f /etc/yum.repos.d/epel-testing.repo",
-    command => "/bin/sed -i '/enabled/c\enabled = 0 ' /etc/yum.repos.d/epel-testing.repo",
+    onlyif  => "/bin/test -f ${base_dir}epel-testing.repo",
+    command => "/bin/sed -i '/enabled/c\enabled = 0 ' ${base_dir}epel-testing.repo",
   } ->
   # update repos if something has changed. -N checks if the file's
   # timestamp has changed. If yes, it downloads it. 
   exec {'update_rhel7_file':
-    command => "/bin/wget -N $rhel7_link -P /etc/yum.repos.d/",
+    command => "/bin/wget -q -N $rhel7_link -P ${base_dir}",
   } ->
   exec {'update_epel_file':
-    command => "/bin/wget -N $epel_link -P /etc/yum.repos.d/",
+    command => "/bin/wget -q -N $epel_link -P ${base_dir}",
   } ->
   exec {'update_suricata_file':
-    command => "/bin/wget -N $suricata_link -P /etc/yum.repos.d/",
+    command => "/bin/wget -q -N $suricata_link -P ${base_dir}",
   }
 
   class { 'yum_cron':
